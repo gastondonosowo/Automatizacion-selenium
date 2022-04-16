e6b8be6d1d966e56759bb8689b61ed0954daa21e
gastondonosowo <75701041+gastondonosowo@users.noreply.github.com>
2022-04-16 03:46:53 -0400

    Create Hito3D.py

---
+++ Hito3D.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import time
+
+mail = "xofob99898@sartess.com"
+contrasena = "ChaoChao1234_"
+contrasena_nueva = "HolaHola1234_"
+
+driver = webdriver.Chrome(r'C:\Users\gndon\Desktop\chromedriver_win32\chromedriver.exe')
+driver.get("https://familyshop.cl/on/demandware.store/Sites-FamilyShop-Site/es_CL/Login-Show")
+time.sleep(3)
+pathB = driver.find_element_by_xpath('//*[@id="login-form-email"]')
+pathB.send_keys(mail)
+pathB = driver.find_element_by_xpath('//*[@id="login-form-password"]')
+pathB.send_keys(contrasena)
+driver.find_element_by_xpath('//*[@id="login"]/form/button').click()
+time.sleep(3)
+driver.find_element_by_xpath('//*[@id="maincontent"]/div[2]/div[2]/div[1]/div[2]/div[1]/a').click()
+time.sleep(3)
+pathB = driver.find_element_by_xpath('//*[@id="currentPassword"]')
+pathB.send_keys(contrasena)
+pathB = driver.find_element_by_xpath('//*[@id="newPassword"]')
+pathB.send_keys(contrasena_nueva)
+pathB = driver.find_element_by_xpath('//*[@id="newPasswordConfirm"]')
+pathB.send_keys(contrasena_nueva)
+driver.find_element_by_xpath('//*[@id="dwfrm_profile"]/div[4]/div[2]/button').click()
